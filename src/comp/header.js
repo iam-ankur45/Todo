13ececc66c0580be3193f3a4b510763eb648cd54
Ankur Chanderia <49068194+iam-ankur45@users.noreply.github.com>
2021-03-18 12:10:10 +0530

    Add files via upload

---
+++ src/comp/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function header() {
+    return (
+        <div className="card text-center rounded-2">
+            <h1 className="display-3">
+                    TODOS
+            </h1>
+        </div>
+    )
+}
